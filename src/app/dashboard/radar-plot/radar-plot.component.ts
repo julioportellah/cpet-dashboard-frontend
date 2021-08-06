322703a21c9bb126c35b200d27cb13eea72d2b9d
Julio Portella <julio.portella@duke.edu>
2021-08-06 13:00:53 -0400

    JPortella changing for a better dashboard

--- src/app/dashboard/radar-plot/radar-plot.component.ts
+++ src/app/dashboard/radar-plot/radar-plot.component.ts
@@ -5,13 +5,27 @@ import {
   ApexTitleSubtitle,
   ApexChart,
   ApexXAxis,
-  ChartComponent
+  ApexFill,
+  ApexDataLabels,
+  ChartComponent,
+  ApexStroke,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexMarkers
 } from "ng-apexcharts";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   title: ApexTitleSubtitle;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  tooltip: any;
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  colors: string[];
+  yaxis: ApexYAxis;
+  markers: ApexMarkers;
   xaxis: ApexXAxis;
 };
 
@@ -29,18 +43,62 @@ export class RadarPlotComponent{
       series: [
         {
           name: "Series 1",
-          data: [80, 50, 30]
+          data: [35.6, 90, 40]
         }
       ],
       chart: {
         height: 350,
         type: "radar"
       },
+      dataLabels: {
+        enabled: true
+      },
+      plotOptions: {
+        radar: {
+          size: 140,
+          polygons: {
+            strokeColor: "#e9e9e9",
+            fill: {
+              colors: ["#f8f8f8", "#fff"]
+            }
+          }
+        }
+      },
       title: {
-        text: "Basic Radar Chart"
+        text: "Primary Limitation by origin"
+      },
+      colors: ["#FF4560"],
+      markers: {
+        size: 4,
+        colors: ["#fff"],
+        strokeColors: ["#FF4560"],
+        strokeWidth: 2
+      },
+      tooltip: {
+        y: {
+          formatter: function(val:any) {
+            return val;
+          }
+        }
       },
       xaxis: {
-        categories: ["Primary Cardiac", "Primary Pulmonary", "Primary Other Origin"]
+        categories: [
+          "Cardiac",
+          "Pulmonary",
+          "Other",
+        ]
+      },
+      yaxis: {
+        tickAmount: 7,
+        labels: {
+          formatter: function(val:any, i:any) {
+            if (i % 2 === 0) {
+              return val;
+            } else {
+              return "";
+            }
+          }
+        }
       }
     };
    }
