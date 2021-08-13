46a28175af0628ecf2b72db5a839b9696ecb8b57
Julio Portella <julio.portella@duke.edu>
2021-08-13 08:36:48 -0400

    hiding the description panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import {ThemePalette} from '@angular/material/core';
 import { PRIMARY_OUTLET } from '@angular/router';
 
+type Links ={
+  name: string;
+  link: string;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  links = ['About', 'Dashboard'];
+  menu: MenuObject = [
+    { "name": "Dashboard", "link": "Dashboard" ,"active":true},
+    { "name": "SHAP plots", "link": "ShapPlots", "active":false },
+    { "name": "Try it", "link": "TryIt", "active":false },
+    { "name": "About", "link": "About", "active":false }
+  ];
+  links = ['Dashboard','About', 'ShapPlots'];
+  textLinks = ['Dashboard','About', 'SHAP plots'];
   activeLink = this.links[0];
   background: ThemePalette = "primary";
   title = 'cpet-dashboard';
@@ -20,3 +31,4 @@ export class AppComponent {
     this.links.push(`Link ${this.links.length + 1}`);
   }
 }
+type MenuObject = Array<{ name: string, link: string, active:boolean }>;
