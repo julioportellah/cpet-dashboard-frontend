eeada01204f0f40d9c32e68933fdb47a017d8443
Julio Portella <julio.portella@duke.edu>
2021-08-16 19:31:26 -0400

    scores plot updated

--- src/app/dynamic-plot/score-timeline/score-timeline.component.ts
+++ src/app/dynamic-plot/score-timeline/score-timeline.component.ts
@@ -31,10 +31,85 @@ export type ChartOptions = {
   styleUrls: ['./score-timeline.component.css']
 })
 export class ScoreTimelineComponent implements OnChanges {
+  @Input('cardiac-scores') cardiacScores: any =[0,0,0,0,0,0,0];
+  @Input('pulmonary-scores') pulmonaryScores: any=[0,0,0,0,0,0,0];;
+  @Input('other-scores') otherScores: any=[0,0,0,0,0,0,0];;
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>| any;;
 
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Cardiac",
+          data: this.cardiacScores
+        },
+        {
+          name: "Pulmonary",
+          data: this.pulmonaryScores
+        },
+        {
+          name: "Other",
+          data: this.otherScores
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2
+        },
+        toolbar: {
+          show: false
+        }
+      },
+      colors: ["#77B6EA", "#545454",'#D13A1A'],
+      dataLabels: {
+        enabled: true
+      },
+      stroke: {
+        curve: "smooth"
+      },
+      title: {
+        text: "Models' scores over the percent of the session",
+        align: "left"
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      markers: {
+        size: 1
+      },
+      xaxis: {
+        categories: ["40%", "50%", "60%", "70%", "80%", "90%", "100%"],
+        title: {
+          text: "Percent of the Session"
+        }
+      },
+      yaxis: {
+        title: {
+          text: "Odds of a Limitation"
+        },
+        min: 0,
+        max: 100
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "right",
+        floating: true,
+        offsetY: -25,
+        offsetX: -5
+      }
+    };
   }
 
   constructor() { 
